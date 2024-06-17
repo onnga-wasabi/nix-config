dff4072c6c9bb3005a20b2064d3cc458981e8261
onnga-wasabi <yuuto.onnga.23@gmail.com>
2024-06-17 15:23:38 +0900

    :sparkles: feat: なおしたお

--- home/base/shell/git/default.nix
+++ home/base/shell/git/default.nix
@@ -16,6 +16,18 @@ let
           };
       in
       pkgs.commitizen;
+    tig =
+      let
+        pkgs = import
+          (builtins.fetchTarball {
+            url = "https://github.com/NixOS/nixpkgs/archive/e3945057be467f32028ff6b67403be08285ad8c8.tar.gz";
+            sha256 = "0jxwgnraha203yd4vw2n61vd99fgrhrvda374zidx6pw1xj88hhy";
+          })
+          {
+            inherit system;
+          };
+      in
+      pkgs.tig;
   };
 in
 {
@@ -24,7 +36,7 @@ in
     gh
     gitui
     lazygit
-    tig
+    customPkgs.tig
     customPkgs.commitizen
   ];
 
