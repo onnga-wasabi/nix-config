36e95e01f953f18fffb68038acc44cfc08704b7c
onnga-wasabi <yuuto.onnga.23@gmail.com>
2024-07-08 17:49:57 +0900

    :zap: update: update

--- home/base/daemon/wezterm/wezterm.lua
+++ home/base/daemon/wezterm/wezterm.lua
@@ -14,6 +14,29 @@ config.font_size = 16
 config.color_scheme = "One Half Black (Gogh)"
 config.colors = {
 	background = "#1f2329", -- nvim の背景と一緒
+
+	-- https://github.com/Gogh-Co/Gogh/blob/master/themes/One%20Half%20Black.yml
+	ansi = {
+		"#282C34", -- Black (Host)
+		"#E06C75", -- Red (Syntax string)
+		"#98C379", -- Green (Command)
+		"#E5C07B", -- Yellow (Command second)
+		"#61AFEF", -- Blue (Path)
+		"#C678DD", -- Magenta (Syntax var)
+		"#56B6C2", -- Cyan (Prompt)
+		"#DCDFE4", -- White
+	},
+
+	brights = {
+		"#BDBCBD", -- Bright Black
+		"#E06C75", -- Bright Red (Command error)
+		"#98C379", -- Bright Green (Exec)
+		"#E5C07B", -- Bright Yellow
+		"#61AFEF", -- Bright Blue (Folder)
+		"#C678DD", -- Bright Magenta
+		"#56B6C2", -- Bright Cyan
+		"#DCDFE4", -- Bright White
+	},
 }
 
 -- Optoion/Alt
--- home/editor/nvim/lua/plugins/ufo.lua
+++ home/editor/nvim/lua/plugins/ufo.lua
@@ -1,95 +1,95 @@
 return {
-	-- https://github.com/kevinhwang91/nvim-ufo
-	"kevinhwang91/nvim-ufo",
-	event = "BufReadPost",
-	dependencies = {
-		"kevinhwang91/promise-async",
-		{
-			"luukvbaal/statuscol.nvim",
-			config = function()
-				local builtin = require("statuscol.builtin")
-				require("statuscol").setup({
-					relculright = true,
-					segments = {
-						{ text = { "%s" }, click = "v:lua.ScSa" },
-						{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-						{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-					},
-				})
-			end,
-		},
-	},
-	-- https://github.com/kevinhwang91/nvim-ufo/issues/4
-	init = function()
-		-- UFO folding
-		vim.o.foldcolumn = "1" -- '0' is not bad
-		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-		vim.o.foldlevelstart = 99
-		vim.o.foldenable = true
-		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-	end,
-	config = function()
-		local opts = {
-			-- INFO: Uncomment to use treeitter as fold provider, otherwise nvim lsp is used
-			-- provider_selector = function(bufnr, filetype, buftype)
-			--   return { "treesitter", "indent" }
-			-- end,
-			open_fold_hl_timeout = 400,
-			close_fold_kinds = { "imports", "comment" },
-			preview = {
-				win_config = {
-					border = { "", "─", "", "", "", "─", "", "" },
-					winhighlight = "Normal:Folded",
-					winblend = 0,
-				},
-				mappings = {
-					scrollU = "<C-u>",
-					scrollD = "<C-d>",
-					jumpTop = "[",
-					jumpBot = "]",
-				},
-			},
-			provider_selector = function()
-				return { "treesitter", "indent" }
-			end,
-			fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-				local newVirtText = {}
-				local suffix = (" 󰁂 %d "):format(endLnum - lnum)
-				local sufWidth = vim.fn.strdisplaywidth(suffix)
-				local targetWidth = width - sufWidth
-				local curWidth = 0
-				for _, chunk in ipairs(virtText) do
-					local chunkText = chunk[1]
-					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-					if targetWidth > curWidth + chunkWidth then
-						table.insert(newVirtText, chunk)
-					else
-						chunkText = truncate(chunkText, targetWidth - curWidth)
-						local hlGroup = chunk[2]
-						table.insert(newVirtText, { chunkText, hlGroup })
-						chunkWidth = vim.fn.strdisplaywidth(chunkText)
-						-- str width returned from truncate() may less than 2nd argument, need padding
-						if curWidth + chunkWidth < targetWidth then
-							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
-						end
-						break
-					end
-					curWidth = curWidth + chunkWidth
-				end
-				table.insert(newVirtText, { suffix, "MoreMsg" })
-				return newVirtText
-			end,
-		}
-		require("ufo").setup(opts)
-		-- vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-		-- vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-		-- vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
-		-- vim.keymap.set("n", "K", function()
-		-- 	local winid = require("ufo").peekFoldedLinesUnderCursor()
-		-- 	if not winid then
-		-- 		-- vim.lsp.buf.hover()
-		-- 		vim.cmd([[ Lspsaga hover_doc ]])
-		-- 	end
-		-- end)
-	end,
+	-- -- https://github.com/kevinhwang91/nvim-ufo
+	-- "kevinhwang91/nvim-ufo",
+	-- event = "BufReadPost",
+	-- dependencies = {
+	-- 	"kevinhwang91/promise-async",
+	-- 	{
+	-- 		"luukvbaal/statuscol.nvim",
+	-- 		config = function()
+	-- 			local builtin = require("statuscol.builtin")
+	-- 			require("statuscol").setup({
+	-- 				relculright = true,
+	-- 				segments = {
+	-- 					{ text = { "%s" }, click = "v:lua.ScSa" },
+	-- 					{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+	-- 					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+	-- 				},
+	-- 			})
+	-- 		end,
+	-- 	},
+	-- },
+	-- -- https://github.com/kevinhwang91/nvim-ufo/issues/4
+	-- init = function()
+	-- 	-- UFO folding
+	-- 	vim.o.foldcolumn = "1" -- '0' is not bad
+	-- 	vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+	-- 	vim.o.foldlevelstart = 99
+	-- 	vim.o.foldenable = true
+	-- 	vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+	-- end,
+	-- config = function()
+	-- 	local opts = {
+	-- 		-- INFO: Uncomment to use treeitter as fold provider, otherwise nvim lsp is used
+	-- 		-- provider_selector = function(bufnr, filetype, buftype)
+	-- 		--   return { "treesitter", "indent" }
+	-- 		-- end,
+	-- 		open_fold_hl_timeout = 400,
+	-- 		close_fold_kinds = { "imports", "comment" },
+	-- 		preview = {
+	-- 			win_config = {
+	-- 				border = { "", "─", "", "", "", "─", "", "" },
+	-- 				winhighlight = "Normal:Folded",
+	-- 				winblend = 0,
+	-- 			},
+	-- 			mappings = {
+	-- 				scrollU = "<C-u>",
+	-- 				scrollD = "<C-d>",
+	-- 				jumpTop = "[",
+	-- 				jumpBot = "]",
+	-- 			},
+	-- 		},
+	-- 		provider_selector = function()
+	-- 			return { "treesitter", "indent" }
+	-- 		end,
+	-- 		fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+	-- 			local newVirtText = {}
+	-- 			local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+	-- 			local sufWidth = vim.fn.strdisplaywidth(suffix)
+	-- 			local targetWidth = width - sufWidth
+	-- 			local curWidth = 0
+	-- 			for _, chunk in ipairs(virtText) do
+	-- 				local chunkText = chunk[1]
+	-- 				local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+	-- 				if targetWidth > curWidth + chunkWidth then
+	-- 					table.insert(newVirtText, chunk)
+	-- 				else
+	-- 					chunkText = truncate(chunkText, targetWidth - curWidth)
+	-- 					local hlGroup = chunk[2]
+	-- 					table.insert(newVirtText, { chunkText, hlGroup })
+	-- 					chunkWidth = vim.fn.strdisplaywidth(chunkText)
+	-- 					-- str width returned from truncate() may less than 2nd argument, need padding
+	-- 					if curWidth + chunkWidth < targetWidth then
+	-- 						suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+	-- 					end
+	-- 					break
+	-- 				end
+	-- 				curWidth = curWidth + chunkWidth
+	-- 			end
+	-- 			table.insert(newVirtText, { suffix, "MoreMsg" })
+	-- 			return newVirtText
+	-- 		end,
+	-- 	}
+	-- 	require("ufo").setup(opts)
+	-- 	-- vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+	-- 	-- vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+	-- 	-- vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+	-- 	-- vim.keymap.set("n", "K", function()
+	-- 	-- 	local winid = require("ufo").peekFoldedLinesUnderCursor()
+	-- 	-- 	if not winid then
+	-- 	-- 		-- vim.lsp.buf.hover()
+	-- 	-- 		vim.cmd([[ Lspsaga hover_doc ]])
+	-- 	-- 	end
+	-- 	-- end)
+	-- end,
 }
